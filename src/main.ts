ba5436372e627682846838e35ada1a83ed35ba6c
unknown <qingsongdu2002@qq.com>
2022-12-13 23:07:32 +0800

    del

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import { initOicq } from './core/oicq'
 import MessageHandlers from './handler'
 import { loadConfig, validConfigFile } from './util/config'
 import { config } from './config'
-import { initChatGPT } from './core/chat-gpt'
 
 async function main () {
   await validConfigFile()
   Object.assign(config, await loadConfig())
 
-  await initChatGPT()
   initOicq(MessageHandlers)
 }
 
